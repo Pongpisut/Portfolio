8098cca4e4636a2935949f29765e84452f4148b6
Peempongpisut <pongpisut@innovisor.co.th>
2023-09-14 15:58:09 +0700

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,8 +24,8 @@ module.exports = {
         transition3: "#38bdf8",
       },
       backgroundImage: {
-        explosion: `url(/Portfolio/image/bg-explosion.png)`,
-        site: `url(/Portfolio/image/site-bg.svg)`,
+        explosion: `url(${process.env.NEXT_PUBLIC_ASSET_PREFIX}/image/bg-explosion.png)`,
+        site: `url(${process.env.NEXT_PUBLIC_ASSET_PREFIX}/image/site-bg.svg)`,
       },
       animation: {
         "spin-slow": "spin 6s linear infinite",

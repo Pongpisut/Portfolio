cb244e0c169fb0d770a0ac996f76d744b861af4f
Peempongpisut <pongpisut@innovisor.co.th>
2023-09-14 18:43:20 +0700

    deploy

--- components/bulb/Blub.js
+++ components/bulb/Blub.js
@@ -4,13 +4,7 @@ import Image from "next/image";
 const Blub = () => {
   return (
     <div className="absolute -left-32 bottom-0 rotate-12 mix-blend-lighten animate-pulse duration-75 z-0 w-[200px] xl:w-[260px]">
-      <Image
-        src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/image/bulb.png`}
-        width={260}
-        height={200}
-        className="w-full h-full"
-        alt=""
-      />
+      <Image src={`/Portfolio/image/bulb.png`} width={260} height={200} className="w-full h-full" alt="" />
     </div>
   );
 };

c960d638567f2f4c367c0bcbd5348f39e5335f74
Peempongpisut <pongpisut@innovisor.co.th>
2023-09-15 15:33:57 +0700

    deploy

--- components/button/Button.js
+++ components/button/Button.js
@@ -5,12 +5,12 @@ const Button = () => {
   return (
     <>
       <div className="rounded-full border-2 border-sky-500 px-5 py-2 bg-transparent  hover:bg-sky-400 transition-all duration-300 ease-in-out ">
-        <a href={"#"} download={`${conditionPath}/file/cv_pongpisut.pdf`}>
+        <a download href={`${conditionPath}/file/cv_pongpisut.pdf`}>
           Download CV (PDF.)
         </a>
       </div>
       <div className="rounded-full border-2 border-sky-500 px-5 py-2 bg-transparent  hover:bg-sky-400 transition-all duration-300 ease-in-out ">
-        <a href={"#"} download={`${conditionPath}/file/cv_pongpisut.jpg`}>
+        <a download href={`${conditionPath}/file/cv_pongpisut.jpg`}>
           Download CV (JPG.)
         </a>
       </div>

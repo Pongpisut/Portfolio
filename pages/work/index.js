b3129180970a85a69c576bfa71f274e72f96b201
Pongpisut <pongpisut1997@gmail.com>
2023-09-17 01:11:31 +0700

    deploy

--- pages/work/index.js
+++ pages/work/index.js
@@ -5,7 +5,7 @@ import { fadeIn } from "../../variants";
 
 const index = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
+    <div className="h-full bg-primary/30 py-44 flex items-center">
       <Circles />
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">

47c42ea43f4e7f912dcde2b2ecd5f50b585a3271
Peempongpisut <pongpisut@innovisor.co.th>
2023-09-17 18:14:14 +0700

    change image and deploy

--- components/circles/Circles.js
+++ components/circles/Circles.js
@@ -4,7 +4,7 @@ import { conditionPath } from "@/utils/ConstantAll";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-lighten animate-pulse duration-75 z-10">
+    <div className="w-[300px] xl:w-[440px] absolute -right-16 -bottom-2 mix-blend-lighten animate-pulse duration-75 z-10">
       <Image src={`${conditionPath}/image/circles.png`} width={260} height={200} className="w-full h-full" alt="" />
     </div>
   );

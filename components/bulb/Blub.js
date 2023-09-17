72e3c72f1ef61e7d20c4e60db01c6b99b1415bbb
Pongpisut <pongpisut1997@gmail.com>
2023-09-17 10:50:40 +0700

    deploy

--- components/bulb/Blub.js
+++ components/bulb/Blub.js
@@ -4,7 +4,7 @@ import { conditionPath } from "@/utils/ConstantAll";
 const Blub = () => {
   return (
     <div className="absolute -left-32 -bottom-36 rotate-12 mix-blend-lighten animate-pulse duration-75 z-0 w-[200px] xl:w-[220px]">
-      {/* <Image src={`${conditionPath}/image/bulb.png`} width={260} height={200} className="w-full h-full" alt="" /> */}
+      <Image src={`${conditionPath}/image/bulb.png`} width={260} height={200} className="w-full h-full" alt="" />
     </div>
   );
 };
